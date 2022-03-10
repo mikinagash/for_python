8ef5bf8c30dba55f8a7afc9aaee0d0f8447f7739
miki <mikinagash1997@gmail.com>
2022-03-10 19:20:06 +0200

    finish 16q

--- for_python.py
+++ for_python.py
@@ -3,3 +3,90 @@
 # for num in range(0, num+1):
 #     if num % 2 == 1:
 #         print(num, "number not even ")
+
+#6
+# sum = 0
+# for i in range(0,14):
+#     sum +=i
+# print(sum)
+
+#7
+# for i in range(1,11):
+#     for x in range(1,11):
+#         print(x*i,"\t",end='')
+#     print()
+
+#8
+# num1 = input("enter 5 numbers :"))
+# num2 = input("enter 5 numbers :"))
+# for i in range(5):
+#     if num1[i]==num2[i]:
+#         print(num1[i],end = '')
+
+#9
+# num1 = input("enter 5 numbers :")
+# num2 = input("enter 1 number :")
+# for i in range(1):
+#     if num2 in num1:
+#        print(num2,'in', num1)
+
+
+# 10
+# sum = 0
+# for i in range(5):
+#  num = int(input("enter number "))
+#  sum += num
+# print(sum//5)
+
+
+#11
+# for i in range(2,101):
+#  flag = 0
+#  for j in range(2,i):
+#   if i%j == 0:
+#    flag = 1
+#    break
+#  if flag != 1:
+#   print(i,"is prime")
+
+#12
+# for i in range(1,11):
+#     for x in range(1,11):
+#         print(x*i,"\t",end='')
+#     print()
+
+#13
+# for i in range(100,5000,4):
+#  print(i)
+
+#14
+# for i in range(5,2501):
+#  if i%6==0:
+#   print(i,"you are prime")
+
+#A
+# for i in range(100,501):
+#  if i == 287:
+#   print(i)
+#   break
+
+#B
+# for i in range(10,21):
+#  if i == 13:
+#   continue
+#  print(i)
+
+#15
+# numbers = input("enter numbers : ")
+# for i in range(int(1)):
+#  print(numbers)
+
+#16
+# sum = 0
+# for i in range(5):
+#  num = int(input("enter number "))
+#  sum += num
+# print(sum//5)
+
+
+
